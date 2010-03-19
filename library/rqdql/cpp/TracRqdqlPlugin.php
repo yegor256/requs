3beedb5974a308338c7a7374b65f8e7e8c78329c
Yegor Bugayenko <yegor@tpc2.com>
2010-03-19 10:26:25 +0000

    trac hook introduced

---
+++ library/rqdql/cpp/TracRqdqlPlugin.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+/**
+ * @version $Id$
+ */
+
+$dir = dirname(__FILE__);
+
+$content = file_get_contents('php://stdin');
+
+// log it into file
+file_put_contents($dir . '/request.txt', $content);
+
