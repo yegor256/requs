6dc9d3bd68e0d559202a14afcaad57f99e780129
Yegor Bugayenko <yegor@tpc2.com>
2010-03-19 13:54:58 +0000

    php hook improved

--- cpp/trac-hook.sh
+++ cpp/trac-hook.sh
@@ -3,4 +3,13 @@
 # @version $Id$
 #
 
-/usr/local/bin/php /home/technoparkcorp/rqdql/trac-hook.php < /dev/stdin
+if [ -f "/usr/local/bin/php" ]; then
+    PHP="/usr/local/bin/php"
+else
+    if [ -f "/usr/bin/php" ]; then
+        PHP="/usr/bin/php"
+    fi
+fi
+
+DIR=`dirname "$0"`
+$PHP $DIR/trac-hook.php < /dev/stdin
