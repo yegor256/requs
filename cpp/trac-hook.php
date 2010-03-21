949e049dcee9a84a062165c189b845e01b712894
Yegor Bugayenko <yegor@tpc2.com>
2010-03-21 08:27:56 +0000

    exceptions introduced

--- cpp/trac-hook.php
+++ cpp/trac-hook.php
@@ -18,65 +18,65 @@ file_put_contents($dir . '/request.txt', $content);
 // start collecting all error messages
 ob_start();
 
-$lines = explode("\n", $content);
-$comment = $lines[0];
+try {
+    $lines = explode("\n", $content);
+    $comment = $lines[0];
 
-// empty comment shall be disallowed. every comment
-// shall contain a link to the ticket, which motivated the change
-// we don't EXIT here, since the output sent will notify
-// Trac that there are some errors and the page won't be saved
-if (!preg_match('/#\d+/', $comment)) {
-    echo "comment: your comment shall contain a link to a motivating ticket\n";
-}
-
-// split input buffer into pages and lines
-$pages = array();
-for ($i=1; $i<count($lines); $i++) {
-    $name = $lines[$i];
-    $linesNo = intval($lines[$i+1]);
-    $pages[$lines[$i]] = array_slice($lines, $i+2, $linesNo);
-    $i += 2 + $linesNo;
-}
-
-// filter-out lines/pages which are not related to scope
-$scopePages = array();
-$thisPage = null;
-foreach ($pages as $name=>$lines) {
-    $outOfScope = true;
-    foreach ($lines as &$line) {
-        if (preg_match('/^\s*=\s+SRS:.*?\s+=\s*$/s', $line)) {
-            $outOfScope = false;
-        }
-        $replacers = array(
-            '/^\s+\*(.*)$/'         => '${1}', // skip spaces before LIST ITEMS
-            '/^\s+\d+\.(.*)$/'      => '${1}', // skip spaces before ENUMERATE ITEMS
-            '/^\s*(.*)\s*$/'        => '${1}', // remove leading and trailing spaces
-            '/^\s*=+\s+.*?\s+=+\s*$/' => '', // kill headers
-            '/\[\[.*?\]\]/'         => '', // kill meta-includers of Trac
-            '/\[wiki:(.*?)\]/'      => '${1}', // convert wiki names to string
-            '/\[.*?\s(.*?)\]/'      => '${1}', // convert wiki links to strings
-            '/\[(.*?)\]/'           => '${1}', // convert wiki links to strings
-            '/\'{2,3}(.*?)\'{2,3}/' => '${1}', // convert bold and italic to normal text
-        );
-        $line = preg_replace(
-            array_keys($replacers),
-            $replacers,
-            $line
-        );
+    // empty comment shall be disallowed. every comment
+    // shall contain a link to the ticket, which motivated the change
+    // we don't EXIT here, since the output sent will notify
+    // Trac that there are some errors and the page won't be saved
+    if (!preg_match('/#\d+/', $comment)) {
+        throw new Exception('comment: your comment shall contain a link to a motivating ticket');
     }
-    if ($outOfScope) {
-        if (is_null($thisPage)) {
-            $thisPage = false;
+
+    // split input buffer into pages and lines
+    $pages = array();
+    for ($i=1; $i<count($lines); $i++) {
+        $name = $lines[$i];
+        $linesNo = intval($lines[$i+1]);
+        $pages[$lines[$i]] = array_slice($lines, $i+2, $linesNo);
+        $i += 2 + $linesNo;
+    }
+
+    // filter-out lines/pages which are not related to scope
+    $scopePages = array();
+    $thisPage = null;
+    foreach ($pages as $name=>$lines) {
+        $outOfScope = true;
+        foreach ($lines as &$line) {
+            if (preg_match('/^\s*=\s+SRS:.*?\s+=\s*$/s', $line)) {
+                $outOfScope = false;
+            }
+            $replacers = array(
+                '/^\s+\*(.*)$/'         => '${1}', // skip spaces before LIST ITEMS
+                '/^\s+\d+\.(.*)$/'      => '${1}', // skip spaces before ENUMERATE ITEMS
+                '/^\s*(.*)\s*$/'        => '${1}', // remove leading and trailing spaces
+                '/^\s*=+\s+.*?\s+=+\s*$/' => '', // kill headers
+                '/\[\[.*?\]\]/'         => '', // kill meta-includers of Trac
+                '/\[wiki:(.*?)\]/'      => '${1}', // convert wiki names to string
+                '/\[.*?\s(.*?)\]/'      => '${1}', // convert wiki links to strings
+                '/\[(.*?)\]/'           => '${1}', // convert wiki links to strings
+                '/\'{2,3}(.*?)\'{2,3}/' => '${1}', // convert bold and italic to normal text
+            );
+            $line = preg_replace(
+                array_keys($replacers),
+                $replacers,
+                $line
+            );
         }
-    } else {
-        $scopePages[$name] = $lines;
-        if (is_null($thisPage)) {
-            $thisPage = $name;
+        if ($outOfScope) {
+            if (is_null($thisPage)) {
+                throw new Exception();
+            }
+        } else {
+            $scopePages[$name] = $lines;
+            if (is_null($thisPage)) {
+                $thisPage = $name;
+            }
         }
     }
-}
 
-if ($thisPage) {
     // group all lines into one single stream
     $stream = array();
     foreach ($scopePages as $page) {
@@ -96,6 +96,9 @@ if ($thisPage) {
         ),
         $pipes
     );
+    if (!is_resource($proc)) {
+        throw new Exception('failed to start RQDQL');
+    }
     fwrite($pipes[0], implode("\n", $stream));
     fclose($pipes[0]);
     $out = stream_get_contents($pipes[1]);
@@ -141,7 +144,11 @@ if ($thisPage) {
             $message
         );
     }
-}
+} catch (Exception $e) {
+    if ($e->getMessage()) {
+        echo "{$e->getMessage()}\n";
+    }
+}    
 
 // get all lines outputed above
 $output = ob_get_clean();
