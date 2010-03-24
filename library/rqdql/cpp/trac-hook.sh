ee475bbd07ea090130c4af48dad6d9fa784ea156
Yegor Bugayenko <yegor@tpc2.com>
2010-03-24 08:59:20 +0000

    revert back to normal PHP finding, without "which"

--- library/rqdql/cpp/trac-hook.sh
+++ library/rqdql/cpp/trac-hook.sh
@@ -3,7 +3,14 @@
 # @version $Id$
 #
 
-PHP=`which php`
+if [ -f "/usr/local/bin/php" ]; then
+    PHP="/usr/local/bin/php"
+else
+    if [ -f "/usr/bin/php" ]; then
+        PHP="/usr/bin/php"
+    fi
+fi
+
 DIR=`dirname "$0"`
 
 $PHP $DIR/trac-hook.php < /dev/stdin
