cfc3fb7a8af369c6dba9ebed03ad570d010a4180
Yegor Bugayenko <yegor@tpc2.com>
2010-03-24 07:23:37 +0000

    which php introduced

--- library/rqdql/cpp/trac-hook.sh
+++ library/rqdql/cpp/trac-hook.sh
@@ -3,15 +3,9 @@
 # @version $Id$
 #
 
-if [ -f "/usr/local/bin/php" ]; then
-    PHP="/usr/local/bin/php"
-else
-    if [ -f "/usr/bin/php" ]; then
-        PHP="/usr/bin/php"
-    fi
-fi
-
+PHP=`which php`
 DIR=`dirname "$0"`
+
 $PHP $DIR/trac-hook.php < /dev/stdin
 exit 0
 
