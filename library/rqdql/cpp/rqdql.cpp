005c126138f195d0583721f76783bcd6f596c1f2
Yegor Bugayenko <yegor@tpc2.com>
2010-05-04 12:15:30 +0000

    temp commit

--- library/rqdql/cpp/rqdql.cpp
+++ library/rqdql/cpp/rqdql.cpp
@@ -13,7 +13,7 @@
  *
  * @author Yegor Bugayenko <egor@tpc2.com>
  * @copyright Copyright (c) rqdql.com, 2010
- * @version $Id$
+ * @version $Id: rqdql.cpp 1746 2010-05-04 11:47:08Z yegor256@yahoo.com $
  */
 
 #include "rqdql.h"
@@ -24,75 +24,6 @@
 using namespace std;
 
 rqdql::LogLevel rqdql::level = L_ERROR;
-
-int main(int argc, char** argv) {
-    // this option is set in Makefile, when building a project for tests
-    #ifdef RQDQL_DEBUG
-        rqdql::level = rqdql::L_DEBUG;
-    #endif
-
-    // entry log message
-    rqdql::log(rqdql::L_INFO, "rqdql v0.1");
-
-    // convert input stream into rqdql::om::Model class instance
-    yyparse();
-    
-    // rqdql::log(boost::format("%d statements found") % rqdql::scope.size());
-    // model.setScope(rqdql::scope::scope);
-    // cout << model.query("") << endl;
-    
-    // bye-bye log message
-    rqdql::log(rqdql::L_INFO, "end.");
-    
-    return 0;
-}
-
-void yySet(string*& lhs, boost::format rhs) {
-    lhs = new string(rhs.str());
-}
-
-void yySet(string*& lhs, char*& rhs) {
-    lhs = new string(rhs);
-}
-
-void rqdql::log(const std::string& line) {
-    return log(L_DEBUG, line);
-}
-
-void rqdql::log(const boost::format& line) {
-    return log(L_DEBUG, line.str());
-}
-
-void rqdql::log(const rqdql::LogLevel lvl, const boost::format& line) {
-    return log(lvl, line.str());
-}
-
-/**
- * To log a line
- */
-void rqdql::log(const rqdql::LogLevel lvl, const std::string& line) {   
-    std::string label;
-    switch (lvl) {
-        case L_DEBUG:
-            label = "DEBUG";
-            break;
-        case L_VERBOSE:
-            label = "VERB";
-            break;
-        case L_INFO:
-            label = "INFO";
-            break;
-        case L_WARNING:
-            label = "WARN";
-            break;
-        case L_ERROR:
-            label = "ERR";
-            break;
-    }
-    if (lvl >= rqdql::level) {
-        cout << '[' << label << "] " << line << endl;
-    }
-}
 
 /**
  * Called when error is found in parser
