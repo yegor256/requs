d9a66777ef414dd382c001987592785b8a37d752
Yegor Bugayenko <yegor@tpc2.com>
2010-05-14 15:47:07 +0000

    scanner samples are in txt files

--- cpp/test/ScannerTest.cpp
+++ cpp/test/ScannerTest.cpp
@@ -19,6 +19,7 @@
 #include <boost/test/minimal.hpp>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include "rqdql.h"
 #include "Scanner.h"
 #include "Proxy.h"
@@ -43,34 +44,19 @@ void tearDown() {
 
 void testSimpleParsing() {
     setUp();
-    rqdql::Scanner::getInstance().scan(
-        "ActorUser is a \"human being with identity. Also it is our client.\".\n"
-        "ActorUser is an ActorVisitor.\n"
-        "InvestorApplication includes:\n"
-            "ticket: OnlineTicket \"where this application is discussed and approved\"; \n"
-            "email, \n"
-            "agent: ActorAgent; \n"
-            "phone: \"a telephone number\"; and \n"
-            "name; \n"
-            "introduction email: \"text message when account is registered\".\n"
-        "Name of InvestorApplication includes: first name and last name.\n"
-        "UC8.3 where we invest into InvestmentProject: \"something for the future\".\n"
-        "UC8.4 where ActorOperator pays to us: \"we will talk later about it...\".\n"
-        "UC8.1 where somebody speculates:\n"
-            "1. We get from ActorLegacy.\n"
-            "2. We invest into InvestmentProject (the project).\n"
-            "3. Somebody turns into ActorUser (the user).\n"
-            "4. The user \"buys specific units in\" the project.\n"
-            "5. \"We do something strange\".\n"
-            "6. ActorOperator \"when he wants\" pays to us \"when necessary\".\n"
-            "7. \"Every week\" we payearnings to the user.\n"
-            "8. The user transfersunits to ActorUser.\n"
-        "UC8.1 4a) if \"ticket is rejected\":\n"
-            "1. SUD sends rejection email of the application.\n"
-            "2. Failure \"application rejected\".\n"
-        "UC8.1 4a1a) if \"email of the application is empty\":\n"
-            "1. Failure \"anonymous application rejected\".\n"
-    );
+    ifstream f;
+    f.open("test/samples/sample1.txt");
+    string sample;
+    if (!f.is_open()) {
+        throw "failed to open sample file";
+    }
+    while (!f.eof()) {
+        string s;
+        getline(f, s);
+        sample = sample + s + "\n";
+    }
+    f.close();
+    rqdql::Scanner::getInstance().scan(sample);
     proxy::Proxy::getInstance().inject();
     BOOST_CHECK(rqdql::Logger::getInstance().empty());
     tearDown();
