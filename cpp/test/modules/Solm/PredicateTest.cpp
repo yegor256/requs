df73ffda6eecc6c3cd7cbf824b6eac1f7e3e9f50
Yegor Bugayenko <yegor@tpc2.com>
2010-07-16 14:57:14 +0000

    renaming

--- cpp/test/modules/Solm/PredicateTest.cpp
+++ cpp/test/modules/Solm/PredicateTest.cpp
@@ -18,121 +18,118 @@
 #include <string>
 #include <vector>
 #include <boost/algorithm/string/join.hpp>
-#include "Solm/Term.h"
+#include "Solm/Predicate.h"
 using namespace solm;
 using std::string;
 
-void testTerm(const string& t, const string& image = "") {
-    Term term = Term(t);
-    
-    if (!image.empty()) {
-        BOOST_CHECK_EQUAL(image, (string)term);
-    }
+void testPredicate(const string& p) {
+    Predicate predicate = Predicate(p);
+    BOOST_CHECK_EQUAL(p, (string)predicate);
     
     std::vector<string> flags;
-    if (term.is(Term::T_ATOM)) {
+    if (predicate.is(Predicate::T_ATOM)) {
         flags.push_back("A");
     }
-    if (term.is(Term::T_TEXT)) {
+    if (predicate.is(Predicate::T_TEXT)) {
         flags.push_back("T");
     }
-    if (term.is(Term::T_NUMBER)) {
+    if (predicate.is(Predicate::T_NUMBER)) {
         flags.push_back("N");
     }
-    if (term.is(Term::T_RULE)) {
+    if (predicate.is(Predicate::T_RULE)) {
         flags.push_back("R");
     }
-    if (term.is(Term::T_FACT)) {
+    if (predicate.is(Predicate::T_FACT)) {
         flags.push_back("F");
     }
-    if (term.is(Term::T_VARIABLE)) {
+    if (predicate.is(Predicate::T_VARIABLE)) {
         flags.push_back("V");
     }
-    if (term.is(Term::T_OBJECT)) {
+    if (predicate.is(Predicate::T_OBJECT)) {
         flags.push_back("O");
     }
     BOOST_TEST_MESSAGE(
-        t + " --> " + (std::string)term 
+        t + " --> " + (std::string)predicate 
         + " [" + boost::algorithm::join(flags, ",") + "]"
     );
 }
 
-BOOST_AUTO_TEST_SUITE(TermTest)
+BOOST_AUTO_TEST_SUITE(PredicateTest)
 
-BOOST_AUTO_TEST_CASE(testEmptyTermWorksFine) {
-    Term t;
+BOOST_AUTO_TEST_CASE(testEmptyPredicateWorksFine) {
+    Predicate t;
     BOOST_REQUIRE((bool)t); // it's TRUE by default
 }
 
 BOOST_AUTO_TEST_CASE(testInvalidFormatsRaiseExceptions) {
-    BOOST_CHECK_THROW(testTerm("true"), std::exception); // DOT missed
-    BOOST_CHECK_THROW(testTerm("123true."), std::exception); // invalid name of the object
-    BOOST_CHECK_THROW(testTerm("-Aksel."), std::exception); // invalid name
+    BOOST_CHECK_THROW(testPredicate("true"), std::exception); // DOT missed
+    BOOST_CHECK_THROW(testPredicate("123true."), std::exception); // invalid name of the object
+    BOOST_CHECK_THROW(testPredicate("-Aksel."), std::exception); // invalid name
 
-    // now let's make sure that we can successfuly parse terms
+    // now let's make sure that we can successfuly parse predicates
     // after the problems happened before
-    testTerm("like(klaus, beer)."); 
+    testPredicate("like(klaus, beer)."); 
 }
 
 BOOST_AUTO_TEST_CASE(testDifferentFormatsAreOK) {
     // simple objects and atoms
-    testTerm("true.", "true");
-    testTerm("mary.", "mary");
-    testTerm("victor, peter, 44, 'works?'.", "victor, peter, 44, 'works?'");
-    testTerm("45.890.");
-    testTerm("'this is text'.");
+    testPredicate("true.", "true");
+    testPredicate("mary.", "mary");
+    testPredicate("victor, peter, 44, 'works?'.", "victor, peter, 44, 'works?'");
+    testPredicate("45.890.");
+    testPredicate("'this is text'.");
     
     // simple facts
-    testTerm("father_of(john, mary).");
-    testTerm("age_of(smith, 45), true.", "age_of(smith, 45)");
-    testTerm("child_of(emily, parents(alex, alice)).");
-    testTerm("mistique_character(king, 'Persia'), king('Arthur').");
-    testTerm("in_class(john, mary_smith, peter, alex_Wilson_3rd).");
-    testTerm("a = 1, b = 2."/*, "a = 1, b = 2"*/);
-    testTerm("a(b < 4, c =\\= 'test', mary(1, 2))."/*, "a(b < 4, c =\\= 'test', mary(1, 2))"*/);
+    testPredicate("father_of(john, mary).");
+    testPredicate("age_of(smith, 45), true.", "age_of(smith, 45)");
+    testPredicate("child_of(emily, parents(alex, alice)).");
+    testPredicate("mistique_character(king, 'Persia'), king('Arthur').");
+    testPredicate("in_class(john, mary_smith, peter, alex_Wilson_3rd).");
+    testPredicate("a = 1, b = 2."/*, "a = 1, b = 2"*/);
+    testPredicate("a(b < 4, c =\\= 'test', mary(1, 2))."/*, "a(b < 4, c =\\= 'test', mary(1, 2))"*/);
     
     // specific formatting
-    testTerm("child(X, Y) :- parent(Y, X)."); // rule definition
-    testTerm("P < 67."); // number of P is less than 67
-    testTerm("P is 67."); // P should be equal to 67
-    testTerm("X = Y."); // X is the same as Y
-    testTerm("X =:= Y.", "X =:= Y"); // X and Y stand for the same number/value
-    testTerm("X =\\= Y."); // X and Y stand for the different values/numbers
+    testPredicate("child(X, Y) :- parent(Y, X)."); // rule definition
+    testPredicate("P < 67."); // number of P is less than 67
+    testPredicate("P is 67."); // P should be equal to 67
+    testPredicate("X = Y."); // X is the same as Y
+    testPredicate("X =:= Y.", "X =:= Y"); // X and Y stand for the same number/value
+    testPredicate("X =\\= Y."); // X and Y stand for the different values/numbers
     
     // simple questions
-    testTerm("parent(X, mary).", "parent(X, mary)"); // who is a parent of mary?
-    testTerm("prince(X), X =\\= 'Arthur'."); // who is a prince and is not "Arthur"?
+    testPredicate("parent(X, mary).", "parent(X, mary)"); // who is a parent of mary?
+    testPredicate("prince(X), X =\\= 'Arthur'."); // who is a prince and is not "Arthur"?
 }
 
 BOOST_AUTO_TEST_CASE(testDoubleTrueIsIgnored) {
     // this trailing TRUE should be ignored
-    Term t("king(john), true.");
+    Predicate t("king(john), true.");
     BOOST_CHECK_EQUAL("king(john)", (string)t);
 
     // leading and trailing true will be ignored
-    t = Term("true, true, woman(alex), true.");
+    t = Predicate("true, true, woman(alex), true.");
     BOOST_CHECK_EQUAL("woman(alex)", (string)t);
 }
 
 BOOST_AUTO_TEST_CASE(testWeCanResolveSimpleFactsAndRules) {
     // here we add simple prolog-style facts
-    Term t("kid(john, mary).");
+    Predicate t("kid(john, mary).");
     
     // now we're asking for a list of X that satisfy this rule
-    Term a = t / Term("kid(john, X).");
+    Predicate a = t / Predicate("kid(john, X).");
     
-    BOOST_REQUIRE((bool)a); // the term should be positive
+    BOOST_REQUIRE((bool)a); // the predicate should be positive
     // BOOST_REQUIRE_EQUAL("X = mary", (string)a);
 }
 
 BOOST_AUTO_TEST_CASE(testWeCanResolveMoreComplexRules) {
     // here we add simple prolog-style facts
-    Term t("kid(john, mary), kid(john, peter).");
+    Predicate t("kid(john, mary), kid(john, peter).");
     
     // now we're asking for a list of X that satisfy this rule
-    Term a = t / Term("kid(john, X).");
+    Predicate a = t / Predicate("kid(john, X).");
     
-    BOOST_REQUIRE((bool)a); // the term should be positive
+    BOOST_REQUIRE((bool)a); // the predicate should be positive
     // BOOST_REQUIRE_EQUAL("X = mary", (string)a);
 }
 
