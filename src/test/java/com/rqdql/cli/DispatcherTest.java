e7937ed9728de7a00dcc08f4724a26cd3960d8f7
Yegor Bugayenko <yegor@tpc2.com>
2010-12-25 07:49:46 +0000

    refs #1 - more tests

--- src/test/java/com/rqdql/cli/DispatcherTest.java
+++ src/test/java/com/rqdql/cli/DispatcherTest.java
@@ -15,4 +15,10 @@ public class DispatcherTest {
         String xml = new Dispatcher().dispatch(args, input);
     }
 
+    @Test(expected = com.rqdql.Problem.class)
+    public void testDispatchesWithInvalidReporter() throws Exception {
+        String[] args = { "-?", "non-existing", "metrics" };
+        new Dispatcher().dispatch(args, "nothing");
+    }
+
 }
