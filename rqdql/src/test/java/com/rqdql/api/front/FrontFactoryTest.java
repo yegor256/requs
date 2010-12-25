6bd281bb6afaad980bad8abfbf7de9429fcce466
Yegor Bugayenko <yegor@tpc2.com>
2010-12-25 07:06:39 +0000

    refs #1 - more coverage, more tests

--- rqdql/src/test/java/com/rqdql/api/front/FrontFactoryTest.java
+++ rqdql/src/test/java/com/rqdql/api/front/FrontFactoryTest.java
@@ -5,6 +5,8 @@ package com.rqdql.api.front;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
 
 public class FrontFactoryTest {
 
@@ -19,6 +21,22 @@ public class FrontFactoryTest {
     public void testMakesAnAttemplToFindNonExistingReporter() throws Exception {
         FrontFactory factory = new FrontFactory();
         Reporter rep = factory.find("there is no such reporter");
+    }
+
+    @Test
+    public void testImmitatesBrokenJavaClassFile() throws Exception {
+        FrontFactory factory = new FrontFactory();
+        File f = new File("target/classes/com/rqdql/impl/front/Errors.class");
+        assert f.exists();
+        byte[] content = FileUtils.readFileToByteArray(f);
+        FileUtils.writeStringToFile(f, "invalid content");
+        try {
+            factory.find("Errors");
+            fail("Exception expected, but not thrown");
+        } catch (java.lang.ClassFormatError ex) {
+            // it's OK
+        }
+        FileUtils.writeByteArrayToFile(f, content);
     }
 
 }
