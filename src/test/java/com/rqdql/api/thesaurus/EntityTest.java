89d99601b52ae88ed8ef40b9fc9e71082b4c1c31
Yegor Bugayenko <yegor@tpc2.com>
2011-02-10 15:50:17 +0000

    reflector will be implemented in rempl

---
+++ src/test/java/com/rqdql/api/thesaurus/EntityTest.java
@@ -0,0 +1,35 @@
+/**
+ * @version $Id$
+ */
+package com.rqdql.api.thesaurus;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+// import static com.rqdql.Reflector.*; // reflection assertions
+
+/**
+ * @todo #3:1hr The entire test case is ignored since
+ *              the Entity class is not implemented yet (it is absent
+ *              actually). The class has to be implemented and this
+ *              test case has to be enabled.
+ */
+@Ignore
+public class EntityTest {
+
+    @Test
+    public void testValidatesExistenceOfAPIConstructs() throws Exception {
+        // Class entity = Class.forName("com.rqdql.api.thesaurus.Entity");
+        // assertClassIsInterface(entity);
+        // assertClassHasField(entity, "slots");
+        // assertFieldOfType(
+        //     entity.getDeclaredField("slots"),
+        //     "java.util.List<com.rqdql.api.thesaurus.Slot>"
+        // );
+        // assertClassHasField(entity, "predicate");
+        // assertFieldOfType(
+        //     entity.getDeclaredField("predicate"),
+        //     "com.rqdql.api.solm.Predicate"
+        // );
+    }
+
+}
---
+++ src/test/java/com/rqdql/api/thesaurus/UseCaseTest.java
@@ -0,0 +1,26 @@
+/**
+ * @version $Id$
+ */
+package com.rqdql.api.thesaurus;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+// import static com.rqdql.Reflector.*; // reflection assertions
+
+/**
+ * @todo #3:1hr The entire test case is ignored since
+ *              the UseCase class is not implemented yet (it is absent
+ *              actually). The class has to be implemented and this
+ *              test case has to be enabled.
+ */
+@Ignore
+public class UseCaseTest {
+
+    @Test
+    public void testValidatesExistenceOfAPIConstructs() throws Exception {
+        // Class uc = Class.forName("com.rqdql.api.thesaurus.UseCase");
+        // assertClassIsInterface(uc);
+        // assertClassIsChildOf(uc, "com.rqdql.api.thesaurus.Entity");
+    }
+
+}
