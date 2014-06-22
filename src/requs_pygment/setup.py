07e73b3f4e0c228399d038b23e0be034fca32cad
Yegor Bugayenko <yegor@tpc2.com>
2014-06-22 13:41:42 +0200

    renamed

--- src/requs_pygment/setup.py
+++ src/requs_pygment/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 setup(
-    name = 'requs-pygment',
+    name = 'requs_pygment',
     version = '0.1',
     packages = find_packages(),
     install_requires = ['pygments'],
@@ -13,7 +13,7 @@ setup(
     url = 'http://www.requs.org/',
     entry_points = {
         'pygments.lexers': [
-            'requs = requs:RequsLexer'
+            'requs = requs_pygment:requs.RequsLexer'
         ]
     }
 )
