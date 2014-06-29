2a531334cb9151ad5edb690024c8cfcf38e8c5c4
Yegor Bugayenko <yegor@tpc2.com>
2014-06-29 13:12:34 +0200

    #20 typo

--- requs-core/src/main/java/org/requs/facet/uml/Plant.java
+++ requs-core/src/main/java/org/requs/facet/uml/Plant.java
@@ -67,7 +67,6 @@ public final class Plant {
     public static String svg(final String src) throws IOException {
         final SourceStringReader reader = new SourceStringReader(src);
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        System.out.println(src);
         reader.generateImage(baos, new FileFormatOption(FileFormat.SVG));
         return new XMLDocument(
             new String(baos.toByteArray())
